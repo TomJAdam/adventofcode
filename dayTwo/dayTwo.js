cbd431bdfaa2aa8c05197cead77a0d255bbe8cc0
Tom Adam <tom.adam21@gmail.com>
2020-12-10 10:41:51 -0800

    solved part two

--- dayTwo/dayTwo.js
+++ dayTwo/dayTwo.js
@@ -1,21 +1,16 @@
 const fs = require("fs");
 const input = fs.readFileSync(`${__dirname}/input.txt`, "utf-8").split("\n");
 
-const test = [
-  "3-6 s: ssdsssss",
-  "17-19 f: cnffsfffzhfnsffttms",
-  "8-11 c: tzvtwncnwvwttp",
-];
-
-function policyCheck(data) {
-  let totalCount = 0;
+const reformatArray = (arr) => {
   let re = /[.*+\-?:$ {}()|[\]\\]/g;
+  return arr.map((item) => item.split(re));
+};
 
-  const reformatArray = data.map((item) => {
-    return item.split(re);
-  });
+// part one
+function policyCheckOne(data) {
+  let totalCount = 0;
 
-  reformatArray.forEach((item) => {
+  reformatArray(data).forEach((item) => {
     const password = item[4];
     let count = 0;
     for (let i = 0; i < password.length; i++) {
@@ -30,4 +25,28 @@ function policyCheck(data) {
   console.log("totalCount :", totalCount);
 }
 
-policyCheck(input);
+policyCheckOne(input);
+
+// part two
+function policyCheckTwo(data) {
+  let totalCount = 0;
+
+  reformatArray(data).forEach((item) => {
+    const password = item[4];
+
+    if (
+      password[item[0] - 1] === item[2] &&
+      password[item[1] - 1] === item[2]
+    ) {
+      null;
+    } else if (password[item[1] - 1] === item[2]) {
+      totalCount += 1;
+    } else if (password[item[0] - 1] === item[2]) {
+      totalCount += 1;
+    }
+  });
+
+  console.log("totalCount :", totalCount);
+}
+
+policyCheckTwo(input);
